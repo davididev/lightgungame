7bd5796f846a46572978bbc9f97df44359bd4397
davididev <davidwaggener@gmail.com>
2024-05-24 13:51:26 -0400

    Updated PlayerUI to have six health
    4 isn't going to be enough.
    I also added a test coroutine to show different possible inputs to the set_health and set_score.  We will be removing it later.

--- Prefabs/PlayerUI.gd
+++ Prefabs/PlayerUI.gd
@@ -3,17 +3,40 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_health(4);
+	set_health(6);
 	set_score(0);
-	pass # Replace with function body.
+	test_health();
 
-
+#Delete this coroutine later
+func test_health():
+	set_health(6);
+	set_score(0);
+	await get_tree().create_timer(2.0).timeout;
+	set_health(5);
+	set_score(2);
+	await get_tree().create_timer(2.0).timeout;
+	set_health(4);
+	set_score(99);
+	await get_tree().create_timer(2.0).timeout;
+	set_health(3);
+	set_score(120);
+	await get_tree().create_timer(2.0).timeout;
+	set_health(2);
+	set_score(200);
+	await get_tree().create_timer(2.0).timeout;
+	set_health(1);
+	set_score(4333);
+	await get_tree().create_timer(2.0).timeout;
+	set_health(0);
+	set_score(54333);
+	await get_tree().create_timer(2.0).timeout;
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func set_health(h : int):
-	for n in range(0, 4, 1):
+	for n in range(0, 6, 1):
 		#var format_string = "HealthBG/Unit%n";
 		var actual_string = str("HealthBG/Unit", n);
 		get_node(actual_string).visible = ((h-1) >= n);
