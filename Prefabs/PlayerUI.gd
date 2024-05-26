9281bcb37efdbcf4ec9976d1b285431d9cce3259
davididev <davidwaggener@gmail.com>
2024-05-26 14:20:46 -0400

    Updated UI
    Added a forcefield bar and updated the Health units to look nicer.

--- Prefabs/PlayerUI.gd
+++ Prefabs/PlayerUI.gd
@@ -11,24 +11,31 @@ func _ready():
 func test_health():
 	set_health(6);
 	set_score(0);
+	set_forcefield_perc(0.0);
 	await get_tree().create_timer(2.0).timeout;
 	set_health(5);
 	set_score(2);
+	set_forcefield_perc(0.1);
 	await get_tree().create_timer(2.0).timeout;
 	set_health(4);
 	set_score(99);
+	set_forcefield_perc(0.2);
 	await get_tree().create_timer(2.0).timeout;
 	set_health(3);
 	set_score(120);
+	set_forcefield_perc(0.3);
 	await get_tree().create_timer(2.0).timeout;
 	set_health(2);
 	set_score(200);
+	set_forcefield_perc(0.4);
 	await get_tree().create_timer(2.0).timeout;
 	set_health(1);
 	set_score(4333);
+	set_forcefield_perc(0.5);
 	await get_tree().create_timer(2.0).timeout;
 	set_health(0);
 	set_score(54333);
+	set_forcefield_perc(0.6);
 	await get_tree().create_timer(2.0).timeout;
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,3 +50,9 @@ func set_health(h : int):
 
 func set_score(s : int):
 	get_node("ScoreLabel").text = "Score: %010d" % [s]
+
+
+func set_forcefield_perc(perc : float):
+	var s = get_node("ForcefieldChargeBG/ForcefieldChargeFG").size;
+	s.x = 150 * perc;
+	get_node("ForcefieldChargeBG/ForcefieldChargeFG").size = s;
