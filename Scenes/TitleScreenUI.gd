1a86fd5fd002b613a7f824b7a4e5fa25b5f6cc88
davididev <davidwaggener@gmail.com>
2024-05-27 15:33:23 -0400

    Added new fonts and title screen
    Also updated UI to use fonts

---
+++ Scenes/TitleScreenUI.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Player.Score = 0;  #Reset score when we go to the beginning of the game, may be reloaded later
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_quit_button_down():
+	get_tree().quit();
+
+
+func _on_button_test_level_1_button_down():
+	get_tree().change_scene_to_file("res://Scenes/Bonus1.tscn");
