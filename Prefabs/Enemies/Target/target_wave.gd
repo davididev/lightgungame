fb8f1fefb42af4ab8808993d8959d26544bcacc0
davididev <davidwaggener@gmail.com>
2024-06-05 18:24:36 -0400

    Added wave enemy to game

--- Prefabs/Enemies/Target/target_wave.gd
+++ Prefabs/Enemies/Target/target_wave.gd
@@ -1,25 +1,41 @@
 extends RigidBody2D
-
-@export var JumpForce = 400.0;
 @export var PointsOnShoot = 1;
 @export var TargetExplosion : PackedScene;
+@export var DegreesPerSecond : float = 180.0;
+@export var WaveHeight : float = 128.0;
+@export var MoveXPerSecond : float = 256.0;
+
+var current_degrees = 0.0;
 
 signal on_shot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_impulse(Vector2.UP * JumpForce);
-	#test_damage();
 	pass # Replace with function body.
-	
-func test_damage():
-	await get_tree().create_timer(2.0).timeout;
-	Player.Damage(1);
 
+var begun = false;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if begun:
+		var current_rad = deg_to_rad(current_degrees);
+		var vel : Vector2;
+		vel.x = -MoveXPerSecond;
+		vel.y = cos(current_rad) * WaveHeight;
+		linear_velocity = vel;
+		current_degrees += DegreesPerSecond * delta;
+		if current_degrees > 360.0:
+			current_degrees -= 360.0;
+	
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	begun = true;
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	if begun:  #Don't delete it unless it has started to move
+		queue_free();
 
 
 func _on_on_shot():
@@ -32,4 +48,3 @@ func _on_on_shot():
 	instance2.global_position = global_position;
 	get_tree().root.get_node("Node2D").add_child(instance2);
 	queue_free()
-	#TODO: Add particle system and points
