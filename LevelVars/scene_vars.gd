78f042ea61a97e57ad0a866ecf2d6c2d69cd7112
davididev <davidwaggener@gmail.com>
2024-06-05 22:20:13 -0400

    Some tweaks
    Updated scene vars to transition to next level
    (need to add something for "level complete")
    Also added checks for instances that aren't valid in ammo box

--- LevelVars/scene_vars.gd
+++ LevelVars/scene_vars.gd
@@ -2,11 +2,15 @@ extends Node2D
 class_name SceneVars
 @export var steps : Array[LevelStep]
 @export var song_name : String = "Bonus"; 
+@export var ending_x = 6400.0;
+@export var next_scene = "Level1";
 
 static var MoveSpeed = 32.0;
+static var EndedLevel : bool = false;
 var current_step = -1;
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	EndedLevel = false;
 	PlayMusic.PlaySong(song_name, get_tree());
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +24,7 @@ func _process(delta):
 		else:
 			SceneVars.MoveSpeed = steps[n].horizontal_speed
 	
+	if current_x > ending_x:
+		SaveData.CurrentLevel = next_scene;
+		EndedLevel = true;
+		get_tree().change_scene_to_file("res://Scenes/Loading.tscn");
--- Prefabs/Level Objs/AmmoBox.gd
+++ Prefabs/Level Objs/AmmoBox.gd
@@ -25,37 +25,44 @@ func _process(delta):
 var first_visible = false;
 
 func _on_visible_on_screen_enabler_2d_screen_entered():
-	first_visible = true;
-	ref_to_sprite.play(&"default");
-	var v = Vector2(-forceLeft, -forceUp);
-	gravity_scale = gravity_scale_on_seen;
-	apply_impulse(v);
+	if is_instance_valid(self):
+		first_visible = true;
+		if is_instance_valid(ref_to_sprite):
+			ref_to_sprite.play(&"default");
+		var v = Vector2(-forceLeft, -forceUp);
+		gravity_scale = gravity_scale_on_seen;
+		apply_impulse(v);
 
 
 
 func _on_on_shot():
-	if ref_to_sprite.visible == true:  #Haven't shot it yet
-		ref_to_sprite.visible = false;
-		var part = ExplosionParticle.instantiate();
-		part.global_position = global_position;
-		add_child(part);
+	if is_instance_valid(ref_to_sprite):
+		if ref_to_sprite.visible == true:  #Haven't shot it yet
+			ref_to_sprite.visible = false;
+			var part = ExplosionParticle.instantiate();
+			part.global_position = global_position;
+			add_child(part);
 		
-		if ammoID == 1:  #Machine gun
-			SaveData.Ammo1 += ammoAmount;
-			ref_to_label.text = "Machine Gun + %d" % ammoAmount;
-		if ammoID == 2:  #Laser cross 
-			SaveData.Ammo2 += ammoAmount;
-			ref_to_label.text = "Laser Cross + %d" % ammoAmount;
-		if ammoID == 3:  #Laser cross 
-			SaveData.Ammo3 += ammoAmount;
-			ref_to_label.text = "Explosion + %d" % ammoAmount;
-		PlayerUI.Update_Ammo_UI = true;
-		gravity_scale = 0.0;
-		linear_velocity = Vector2.ZERO;
-		await get_tree().create_timer(1.5).timeout;
-		queue_free();
+			if ammoID == 1:  #Machine gun
+				SaveData.Ammo1 += ammoAmount;
+				if is_instance_valid(ref_to_label):
+					ref_to_label.text = "Machine Gun + %d" % ammoAmount;
+			if ammoID == 2:  #Laser cross 
+				SaveData.Ammo2 += ammoAmount;
+				if is_instance_valid(ref_to_label):
+					ref_to_label.text = "Laser Cross + %d" % ammoAmount;
+			if ammoID == 3:  #Laser cross 
+				SaveData.Ammo3 += ammoAmount;
+				if is_instance_valid(ref_to_label):
+					ref_to_label.text = "Explosion + %d" % ammoAmount;
+			PlayerUI.Update_Ammo_UI = true;
+			gravity_scale = 0.0;
+			linear_velocity = Vector2.ZERO;
+			await get_tree().create_timer(1.5).timeout;
+			queue_free();
 
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	if first_visible:  #it appeared and flew, delete it now that it's off screen
+		await get_tree().create_timer(0.5).timeout;  #but wait 0.5 seconds just in case it was destroyed shortly after being created
 		queue_free();
